2cd69e77f914e3f46f415888c428d4f9895e6491
Angel Vilches U <angel.vilches@usach.cl>
2022-11-01 12:54:36 -0300

    config-server

---
+++ src/main/java/com/mueblesStgoCS/mueblesStgoCS/MueblesStgoCsApplication.java
@@ -0,0 +1,15 @@
+package com.mueblesStgoCS.mueblesStgoCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MueblesStgoCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblesStgoCsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mueblesStgoCS/mueblesStgoCS/MueblesStgoCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mueblesStgoCS.mueblesStgoCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesStgoCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
